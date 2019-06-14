abb9ec00e4da3c63ff10c4807b6e459a99dfc842
yurevych <aleksej.yurevich@gmail.com>
2019-06-14 16:27:46 +0300

    nw

--- src/main/java/imdb_parce/ParseFilmPage.java
+++ src/main/java/imdb_parce/ParseFilmPage.java
@@ -1,5 +1,6 @@
 package imdb_parce;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -10,21 +11,23 @@ import org.jsoup.select.Elements;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class ParseFilmPage {
 
     @Test
     public void parsePage() throws IOException {
+
+        Map<String,String> directorRate= new HashMap<>();
         OkHttpClient client = new OkHttpClient();
         Request request = new Request.Builder().url("https://www.imdb.com/chart/top?ref_=nv_mv_250").build();
         Response response = client.newCall(request).execute();
         String html = response.body().string();
         List<FilmPage> filmPages = new ArrayList<>();
         List<String> topfilms = new ArrayList<>();
+        List<String> directors = new ArrayList<>();
         Document document = Jsoup.parse(html);
         Elements elements = document.select(".titleColumn a");
         for (Element element : elements) {
@@ -33,7 +36,10 @@ public class ParseFilmPage {
             topfilms.add(filmsUrls);
         }
 
-        for (int i = 0; i < 10; i++) {
+
+
+
+        for (int i = 0; i < 30; i++) {
             String urls = topfilms.get(i);
 
             Request request1 = new Request.Builder().url("https://www.imdb.com" + urls + "\"").build();
@@ -46,17 +52,43 @@ public class ParseFilmPage {
                 double rating = Double.parseDouble(element.select("span[itemprop='ratingValue']").text());
                 String director = element.select(".plot_summary :nth-child(2) > a").text();
                 String urlDirector = element.select(".plot_summary :nth-child(2) > a").attr("href");
-                int metascore = Integer.parseInt(element.select(".metacriticScore > span").text());
+                String a = element.select(".metacriticScore > span").text();
+                if (a.equals("")) {
+                    a = "0";
+                }
+                int metascore = Integer.parseInt(a);
                 int year = Integer.parseInt(element.select("#titleYear > a").text());
                 String actors = element.select(".plot_summary :nth-child(4) :nth-child(2)").text();
                 String genres = element.select(".subtext :nth-child(4)").text();
                 String r1 = element.select(".txt-block > time[datetime]").text();
+                if (r1.equals("")) {
+                    r1 = "120 min";
+                }
+                System.out.println(r1);
+                System.out.println(urls);
+
                 int runtime = Integer.parseInt(r1.substring(0, r1.indexOf("m") - 1));
+                directors.add(director);
                 filmPages.add(new FilmPage(filmName, rating, director, metascore, year, actors, genres, runtime, urlDirector));
+
+
             }
 
 
         }
+        HashMap<String, Integer> hm = new HashMap<String, Integer>();
+        Integer item;
+        for (String wrd : directors) {
+
+
+            item = hm.get(wrd);
+            if (item == null) hm.put(wrd, 1); // если нет в списке то добавить со значением 1
+            else hm.put(wrd, item + 1); // если есть такая фамилия(Key), то +1
+        }
+
+        System.out.println(hm);
+        System.out.println("************************************");
+
         /* 1. Отсортировать список фильмов по году выхода и вывести в консоль */
         Collections.sort(filmPages, new SortByYear());
         for (FilmPage filmPage : filmPages) {
@@ -75,6 +107,21 @@ public class ParseFilmPage {
         Collections.sort(filmPages, new SortByRuntime());
         System.out.println(filmPages.get(0));
         System.out.println("************************************");
+
+
+
+
+        Set<String> directorsList = filmPages.stream().map(film -> film.director).collect(Collectors.toSet());
+        for(String i:directorsList){
+            System.out.println("********************1");
+            System.out.println(i);
+            System.out.println("********************1");
+        }
+
+
+
+
+        directorRate.put("","");
 
 
     }
