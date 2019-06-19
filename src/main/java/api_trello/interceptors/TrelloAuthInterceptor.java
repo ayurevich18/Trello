6b024d59a913998099da4715da8b6047a5185ea6
alekseyyurevich <aleksej.yurevich@gmail.com>
2019-06-19 20:10:18 +0300

    delete token

--- src/main/java/api_trello/interceptors/TrelloAuthInterceptor.java
+++ src/main/java/api_trello/interceptors/TrelloAuthInterceptor.java
@@ -8,8 +8,8 @@ import okhttp3.Response;
 import java.io.IOException;
 
 public class TrelloAuthInterceptor implements Interceptor {
-    private static final String KEY = "99b091ca4e8d6cbc4e6305fb4349e363";
-    private static final String TOKEN = "ca72e70f16196e4fc4ca468b54018eaa2fa7a8d337d54de83177f338258fabad";
+    private static final String KEY = "";
+    private static final String TOKEN = "";
 
 
     @Override
